719a7575916f793b0e23965e57915dce8a64f9d8
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-29 11:00:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,80 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) {
+  const employee = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return employee;
+}
+
+function createEmployeeRecords(array) {
+  const employees = array.map((event) => createEmployeeRecord(event));
+  return employees;
+}
+
+function createTimeInEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  let timeInEntries = {
+    type: "TimeIn",
+    hour: parseInt(hour),
+    date: date,
+  };
+  this.timeInEvents.push(timeInEntries);
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  const timeOutEvent = {
+    type: "TimeOut",
+    hour: parseInt(hour),
+    date: date,
+  };
+  this.timeOutEvents.push(timeOutEvent);
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find((day) => day.date === date);
+  const timeOut = this.timeOutEvents.find((day) => day.date === date);
+
+  const timeInHr = timeIn.hour;
+  const timeOutHr = timeOut.hour;
+
+  const hoursWorked = (timeOutHr - timeInHr) / 100;
+  return hoursWorked;
+}
+
+function wagesEarnedOnDate(date) {
+  const hours = hoursWorkedOnDate.call(this, date);
+  const payOwed = hours * this.payPerHour;
+  return payOwed;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find((array) => array.firstName === firstName);
+}
+function calculatePayroll(employees) {
+  return employees.reduce((payRoll, employee) => {
+    payRoll + allWagesFor.call(employee);
+  }, 0);
+}
+
+function calculatePayroll(employeeRecords) {
+    let totalPay = 0;
+    employeeRecords.forEach((record) => {
+      totalPay += allWagesFor.call(record);
+    });
+    return totalPay;
+  }
+  
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -10,14 +85,16 @@
  */
 
 const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  const eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  const payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
-}
-
+  return payable;
+};
